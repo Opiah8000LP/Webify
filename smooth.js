6c59087a056b2817ec2997a87ff8de540db6eb4b
Op <113507103+Opiah8000LP@users.noreply.github.com>
2025-02-22 16:45:10 +0200

    Update and rename smooth.js to Login.css

--- smooth.js
+++
@@ -1,7 +0,0 @@
-// scripts.js
-
-window.addEventListener('load', () => {
-    document.querySelector('.cta-button').addEventListener('click', () => {
-        alert('Enjoy the music!');
-    });
-});
